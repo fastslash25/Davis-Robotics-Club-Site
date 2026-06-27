f3eb5e88a8021d8e2037da4a85358d63a40ff506
Jacob Denny <fastslash25@gmail.com>
2026-06-27 14:21:31 -0700

    configure github pages url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://yourusername.github.io',
-  base: '/',
+  site: 'https://fastslash25.github.io/Davis-Robotics-Club-Site',
+  base: '/Davis-Robotics-Club-Site/',
   output: 'static',
 });
